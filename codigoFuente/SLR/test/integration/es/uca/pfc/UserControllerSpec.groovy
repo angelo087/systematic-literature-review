b5efe3965314c9e7293a64e9282f40e336b21e5e
angelo087 <angelo087@37da72fb-f068-4fa1-ad69-c8d2b3af2960>
2017-06-05 15:31:54 +0000

    - Test integration para UserController

--- codigoFuente/SLR/test/integration/es/uca/pfc/UserControllerSpec.groovy
+++ codigoFuente/SLR/test/integration/es/uca/pfc/UserControllerSpec.groovy
@@ -22,13 +22,43 @@ class UserControllerSpec extends Specification {
         def superRole = Role.findByAuthority('ROLE_SUPER') ?: new Role(authority: 'ROLE_SUPER').save(failOnError: true)
 		
 		// Create profiles
-		def profile01 = new UserProfile(
-			first_name: 'Paco',
-			last_name: 'Jimenez',
-			display_name: 'Paco Jimenez',
-			url_foto: 'http://fotos00.laopiniondemurcia.es/fotos/noticias/318x200/2011-11-02_IMG_2011-11-02_21:57:56_00902murmu.jpg')
+		def profileSuperAdmin = new UserProfile(
+			first_name: 'Super Admin',
+			last_name: 'User',
+			display_name: 'Super Admin User',
+			url_foto: 'http://www.free-icons-download.net/images/user-icon-44709.png')
 		
-		def mendProfile01 = new UserMendeley(
+		def profileAdmin = new UserProfile(
+			first_name: 'Admin',
+			last_name: 'User',
+			display_name: 'Admin User',
+			url_foto: 'http://www.free-icons-download.net/images/user-icon-44709.png')
+		
+		def profileUser = new UserProfile(
+			first_name: 'User',
+			last_name: 'No Admin No Super',
+			display_name: 'User No Admin No Super',
+			url_foto: 'http://www.free-icons-download.net/images/user-icon-44709.png')
+		
+		def mendProfileSuperAdmin = new UserMendeley(
+			email_mend: 'email',
+			pass_mend: 'pass',
+			access_token: 'accesstoken',
+			token_type: 'tokentype',
+			expires_in: 'expiresin',
+			refresh_token: 'refreshtoken'
+			)
+		
+		def mendProfileAdmin = new UserMendeley(
+			email_mend: 'email',
+			pass_mend: 'pass',
+			access_token: 'accesstoken',
+			token_type: 'tokentype',
+			expires_in: 'expiresin',
+			refresh_token: 'refreshtoken'
+			)
+		
+		def mendProfileUser = new UserMendeley(
 			email_mend: 'email',
 			pass_mend: 'pass',
 			access_token: 'accesstoken',
@@ -39,12 +69,32 @@ class UserControllerSpec extends Specification {
 		
 		superAdmin = User.findByUsername('superadmin@uca.es') ?: new User(
 			username: 'superadmin@uca.es',
-			password: 'superadmin',
-			userProfile: profile01,
-			userMendeley: mendProfile01).save(failOnError: true)
+			password: 'superadminpassword',
+			userProfile: profileSuperAdmin,
+			userMendeley: mendProfileSuperAdmin).save(failOnError: true)
 		
 		if (!superAdmin.authorities.contains(superRole)) {
 			UserRole.create superAdmin, superRole
+		}
+		
+		admin = User.findByUsername('admin@uca.es') ?: new User(
+			username: 'admin@uca.es',
+			password: 'adminpassword',
+			userProfile: profileAdmin,
+			userMendeley: mendProfileAdmin).save(failOnError: true)
+		
+		if (!admin.authorities.contains(adminRole)) {
+			UserRole.create admin, adminRole
+		}
+		
+		user = User.findByUsername('user@uca.es') ?: new User(
+			username: 'user@uca.es',
+			password: 'userpassword',
+			userProfile: profileAdmin,
+			userMendeley: mendProfileAdmin).save(failOnError: true)
+		
+		if (!user.authorities.contains(userRole)) {
+			UserRole.create user, userRole
 		}
     }
 
@@ -58,6 +108,25 @@ class UserControllerSpec extends Specification {
 			def model = controller.list()
 		then:
 			model.userListInstance != null
-			model.userListInstance.size() > 1
+			model.userListInstance.size() >= 1
+	}
+	
+	void "test user list by administrator"() {
+		given:
+			controller.springSecurityService.reauthenticate(admin.username, admin.password)
+		when:
+			def model = controller.list()
+		then:
+			model.userListInstance != null
+			model.userListInstance.size() >= 1
+	}
+	
+	void "test user list by user"() {
+		given:
+			controller.springSecurityService.reauthenticate(user.username, user.password)
+		when:
+			def model = controller.list()
+		then:
+			model == null
 	}
 }
